db190d31440aa136ed58a394ddbd60ab7ab0e940
gurrium <gurrium@hatena.ne.jp>
2022-10-31 23:57:26 +0900

    init

---
+++ background.js
@@ -0,0 +1,16 @@
+var ids = {}
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  const tab1BasedIndex = tab.index + 1
+  const regexp = new RegExp(`^\\[${tab1BasedIndex}\\] `) 
+
+  if (0 < tab1BasedIndex && tab1BasedIndex < 9 && !regexp.test(tab.title)) {
+    chrome.scripting.executeScript({
+      injection: {
+        target: { tabId: tab.id },
+        args: [tab1BasedIndex, tab.title],
+        func: (index, title) => { alert("hoge"); document.title = `[${index}] ${title}` }
+      }
+    })
+  }
+})
